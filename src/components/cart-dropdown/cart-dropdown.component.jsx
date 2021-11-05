4209753fe9c86d5776913612a08df7414f72154d
Andrei Buiu <andrei@buiu.co.uk>
2021-11-05 14:25:12 +0200

    moved directory state to redux

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,7 +11,7 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems, history, dispatch}) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -22,10 +22,12 @@ const CartDropdown = ({ cartItems, history, dispatch}) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => {
-      history.push("/checkout");
-      dispatch(toggleCartHidden());
-    }}>
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from  './redux/store'
+import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-
 ReactDOM.render(
-      <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
